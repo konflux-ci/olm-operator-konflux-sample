28a62e14071b8e4e90a95276c85a4203e327f321
arewm <arewm@users.noreply.github.com>
2024-07-16 15:29:28 -0400

    Initial commit to modify bundle content
    This adds net-new steps required to modify a bundle image for Konflux.
    This type of flow can be added into any existing processing/tooling for
    bundle generation.
    
    The source CSV already contains the relatedImages section so this
    tooling does not need to take that into account. If needed, a tool like
    https://github.com/containerbuildsystem/operator-manifest can be used to
    generate the relatedImage specification.
    
    By including the pullspecs of all images that will be referenced in the
    CSV in a single file, it will enable these to be easily nudged in the
    future after new builds have completed.
    
    Signed-off-by: arewm <arewm@users.noreply.github.com>

---
+++ bundle-hack/update_bundle.sh
@@ -0,0 +1,68 @@
+#!/usr/bin/env bash
+
+export GATEKEEPER_GATEKEEPER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper@sha256:230ac0a06e8d6c298c522d745bd1f8ebcf47e1210c90157ddb033731fad2524c"
+export GATEKEEPER_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper-operator@sha256:7e0e1c2e5f651c497f6e7c361611a0211590ce04509022ff93fa68f52e7e212c"
+
+export CSV_FILE=/manifests/gatekeeper-operator.clusterserviceversion.yaml
+
+sed -i -e "s|quay.io/gatekeeper/gatekeeper:v.*|\"${GATEKEEPER_GATEKEEPER_IMAGE_PULLSPEC}\"|g" \
+	-e "s|quay.io/gatekeeper/gatekeeper-operator:v.*|\"${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC}\"|g" \
+	"${CSV_FILE}"
+
+export AMD64_BUILT=$(skopeo inspect --raw docker://${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="amd64")')
+export ARM64_BUILT=$(skopeo inspect --raw docker://${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="arm64")')
+export PPC64LE_BUILT=$(skopeo inspect --raw docker://${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="ppc64le")')
+export S390X_BUILT=$(skopeo inspect --raw docker://${GATEKEEPER_OPERATOR_IMAGE_PULLSPEC} | jq -e '.manifests[] | select(.platform.architecture=="s390x")')
+
+export EPOC_TIMESTAMP=$(date +%s)
+# time for some direct modifications to the csv
+python3 - << CSV_UPDATE
+import os
+from collections import OrderedDict
+from sys import exit as sys_exit
+from datetime import datetime
+from ruamel.yaml import YAML
+yaml = YAML()
+def load_manifest(pathn):
+   if not pathn.endswith(".yaml"):
+      return None
+   try:
+      with open(pathn, "r") as f:
+         return yaml.load(f)
+   except FileNotFoundError:
+      print("File can not found")
+      exit(2)
+
+def dump_manifest(pathn, manifest):
+   with open(pathn, "w") as f:
+      yaml.dump(manifest, f)
+   return
+timestamp = int(os.getenv('EPOC_TIMESTAMP'))
+datetime_time = datetime.fromtimestamp(timestamp)
+gatekeeper_csv = load_manifest(os.getenv('CSV_FILE'))
+# Add arch support labels
+gatekeeper_csv['metadata']['labels'] = gatekeeper_csv['metadata'].get('labels', {})
+if os.getenv('AMD64_BUILT'):
+	gatekeeper_csv['metadata']['labels']['operatorframework.io/arch.amd64'] = 'supported'
+if os.getenv('ARM64_BUILT'):
+	gatekeeper_csv['metadata']['labels']['operatorframework.io/arch.arm64'] = 'supported'
+if os.getenv('PPC64LE_BUILT'):
+	gatekeeper_csv['metadata']['labels']['operatorframework.io/arch.ppc64le'] = 'supported'
+if os.getenv('S390X_BUILT'):
+	gatekeeper_csv['metadata']['labels']['operatorframework.io/arch.s390x'] = 'supported'
+gatekeeper_csv['metadata']['labels']['operatorframework.io/os.linux'] = 'supported'
+gatekeeper_csv['metadata']['annotations']['createdAt'] = datetime_time.strftime('%d %b %Y, %H:%M')
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/disconnected'] = 'true'
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/fips-compliant'] = 'true'
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/proxy-aware'] = 'false'
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/tls-profiles'] = 'false'
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-aws'] = 'false'
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-azure'] = 'false'
+gatekeeper_csv['metadata']['annotations']['features.operators.openshift.io/token-auth-gcp'] = 'false'
+gatekeeper_csv['metadata']['annotations']['repository'] = 'https://github.com/stolostron/gatekeeper-operator'
+gatekeeper_csv['metadata']['annotations']['containerImage'] = os.getenv('GATEKEEPER_OPERATOR_IMAGE_PULLSPEC', '')
+
+dump_manifest(os.getenv('CSV_FILE'), gatekeeper_csv)
+CSV_UPDATE
+
+cat $CSV_FILE
