e10cfd7556fcf5e39f9025a409bc0bb9d7ec66b8
red-hat-konflux <123456+red-hat-konflux[bot]@users.noreply.github.com>
2024-08-27 18:53:01 +0000

    Update gatekeeper-operator to 9539680

--- bundle-hack/update_bundle.sh
+++ bundle-hack/update_bundle.sh
@@ -2,7 +2,7 @@
 
 export GATEKEEPER_GATEKEEPER_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper@sha256:4632bc387e54960ce0bb0fdb4fdda89dbc8c7afc16c2427ff6e517fb47379256"
 
-export GATEKEEPER_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper-operator@sha256:aa395edded8dde4a38e1033fa47e2fad6fa0bebc1aa0889d5da47afcb8f34267"
+export GATEKEEPER_OPERATOR_IMAGE_PULLSPEC="quay.io/redhat-user-workloads/konflux-samples-tenant/olm-operator/gatekeeper-operator@sha256:9539680c13deaac90cd6846bd5a39d5ce593eb92b6ce377076de2f09eb9dcc33"
 
 export CSV_FILE=/manifests/gatekeeper-operator.clusterserviceversion.yaml
 
